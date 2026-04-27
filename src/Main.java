0fda25cfac0df8def3efb3dc1b825212b6dec66e
7une9ro <racoon121238@gmail.com>
2026-04-27 14:24:00 +0900

    Thread: Thread - set/getName

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,28 @@
 public class Main {
-
-    // 쓰레드가 공유할 객체
-    private static final Object lock = new Object();
-    private static boolean isMainTurn = true;
-
     public static void main(String[] args){
-        Thread taskThread = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                synchronized (lock) {
-                    try {
-                        while (isMainTurn) {
-                            lock.wait();
-                        }
-                        System.out.println("Task 쓰레드");
+        Thread mainThread = Thread.currentThread();
+        mainThread.setName("Main 쓰레드");
+        System.out.println(mainThread.getName());
 
-                        isMainTurn = true;
-                        lock.notifyAll();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
+        for (int i = 0; i < 3; i++) {
+            Thread threadA = new Thread() {
+                @Override
+                public void run() {
+                    setName("A 쓰레드");
+                    System.out.println(getName());
                 }
-            }
-        });
-
-        taskThread.start();
-
-        // Main 쓰레드
-        for (int i = 0; i < 5; i++) {
-            synchronized (lock) {
-                try {
-                    while (!isMainTurn) {
-                        lock.wait();
-                    }
-                    System.out.println("Main 쓰레드");
-
-                    isMainTurn = false;
-                    lock.notifyAll();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
+            };
+            threadA.start();
         }
+
+        Thread threadB = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName());
+            }
+        };
+
+        threadB.setName("B 쓰레드");
+        threadB.start();
     }
 }

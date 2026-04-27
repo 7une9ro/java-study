c01be70f35973747e86e363417014e179a450ebf
7une9ro <racoon121238@gmail.com>
2026-04-27 11:02:41 +0900

    Thread: Main Thread & Task Thread

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,45 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
-
 public class Main {
+
+    // 쓰레드가 공유할 객체
+    private static final Object lock = new Object();
+    private static boolean isMainTurn = true;
+
     public static void main(String[] args){
+        Thread taskThread = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                synchronized (lock) {
+                    try {
+                        while (isMainTurn) {
+                            lock.wait();
+                        }
+                        System.out.println("Task 쓰레드");
 
-        List<User> userList = new Vector<>();
-//        List<User> userList = new ArrayList<>();
-//        List<User> userList = new LinkedList<>();
-
-
-        Thread threadA = new Thread() {
-            @Override
-            public void run() {
-                for (int i = 1; i <= 1000; i++) {
-                    userList.add(new User("이름" + i, "비번" + i));
+                        isMainTurn = true;
+                        lock.notifyAll();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
             }
-        };
+        });
 
-        Thread threadB = new Thread() {
-            @Override
-            public void run() {
-                for (int i = 1001; i <= 2000; i++) {
-                    userList.add(new User("이름" + i, "비번" + i));
+        taskThread.start();
+
+        // Main 쓰레드
+        for (int i = 0; i < 5; i++) {
+            synchronized (lock) {
+                try {
+                    while (!isMainTurn) {
+                        lock.wait();
+                    }
+                    System.out.println("Main 쓰레드");
+
+                    isMainTurn = false;
+                    lock.notifyAll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
             }
-        };
-
-        threadA.start();
-        threadB.start();
-
-        try {
-            threadA.join();
-            threadB.join();
-        } catch (Exception e) {
         }
-
-        int size = userList.size();
-        System.out.println("생성된 총 User 수: " + size);
     }
 }

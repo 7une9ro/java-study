8cc1636f07e4c1150437da77a8751fda61d6ef9b
7une9ro <racoon121238@gmail.com>
2026-04-28 11:27:01 +0900

    Thread: join()

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        Thread mainThread = Thread.currentThread();
-        mainThread.setName("Main 쓰레드");
-        System.out.println(mainThread.getName());
+        SumThread sumThread = new SumThread();
+        sumThread.start();
 
-        for (int i = 0; i < 3; i++) {
-            Thread threadA = new Thread() {
-                @Override
-                public void run() {
-                    setName("A 쓰레드");
-                    System.out.println(getName());
-                }
-            };
-            threadA.start();
+        try {
+            sumThread.join();
+        } catch (InterruptedException e) {
         }
 
-        Thread threadB = new Thread() {
-            @Override
-            public void run() {
-                System.out.println(getName());
-            }
-        };
-
-        threadB.setName("B 쓰레드");
-        threadB.start();
+        System.out.println("1~100까지의 합: " + sumThread.getSum());
     }
 }
